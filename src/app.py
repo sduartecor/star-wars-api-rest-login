fbb7b7cff5f6c83378141d54c0069f67d3073c6a
sduartecor <santiagoduartecor@outlook.com>
2023-01-26 17:43:07 +0000

    Primer commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People, Vehicle, Favorite
+import json
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +37,568 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# ----- Endpoint User -----
+
+# Get - All Users
+@app.route('/users', methods=['GET'])
+def getUsers():
+
+    users = User.query.all() 
+    all_users = list(map(lambda item: item.serialize(), users))
+
+    return jsonify(all_users), 200
+# Get - One User for ID
+@app.route('/users/<int:id>', methods=['GET'])
+def getUser(id):
+
+    user = User.query.filter_by(id=id).first()
+
+    if user is not None:
+        return jsonify(user.serialize()), 200
+    
+    # Afuera del if
+    response_body = {
+            "msg": "Usuario no existe"
+        }
+    return jsonify(response_body), 400
+
+# Delete - User
+@app.route('/users/<int:id>', methods=['DELETE'])
+def deleteUser(id):
+    user = User.query.filter_by(id=id).first()
+    
+    if user is not None:
+        db.session.delete(user)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Eliminación correcta de Usuario"
+        }
+        return jsonify(response_body), 200
+
+    # Afuera del if
+    response_body = {
+            "msg": "Usuario no existe"
+        }
+    return jsonify(response_body), 400
+
+# Post - User
+@app.route('/users', methods=['POST'])
+def postUser():
+    body = json.loads(request.data)
+    
+    user = User.query.filter_by(email=body["email"]).first() 
+    
+    if user is None:
+        newUser = User(first_name=body["first_name"], last_name=body["last_name"], email=body["email"], password=body["password"], username=body["username"])
+        db.session.add(newUser)
+        db.session.commit()
+
+        response_body = {
+            "msg": "El usuario fue creado con exito"
+        }
+        return jsonify(response_body), 200
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+            "msg": "El usuario ya existe en el sistema"
+        }
+    return jsonify(response_body), 400
 
-    return jsonify(response_body), 200
+# Put - User
+@app.route('/users/<int:id>', methods=['PUT'])
+def putUser(id):
+    body = json.loads(request.data)
+    
+    user = User.query.filter_by(id=id).first()
+    
+    if user is not None:
+        user.first_name = body["first_name"]
+        user.last_name = body["last_name"]
+        user.email = body["email"]
+        user.password = body["password"]
+        user.username = body["username"]
+
+        db.session.add(user)
+        db.session.commit()
+
+        response_body = {
+            "msg": "El usuario fue modificado con exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "El usuario no existe en el sistema"
+        }
+    return jsonify(response_body), 400
+
+# ----- Endpoint People -----
+
+# Get - All People
+@app.route('/people', methods=['GET'])
+def all_people():
+
+    people = People.query.all() 
+    all_people = list(map(lambda item: item.serialize(), people))
+
+    return jsonify(all_people), 200
+
+# Get - One User for ID
+@app.route('/people/<int:id>', methods=['GET'])
+def getPeople(id):
+
+    people = People.query.filter_by(id=id).first()
+
+    if people is not None:
+        return jsonify(people.serialize()), 200
+
+    # Afuera del if
+    response_body = {
+            "msg": "Personaje no existe"
+        }
+    return jsonify(response_body), 400
+
+# Delete - People
+@app.route('/people/<int:id>', methods=['DELETE'])
+def deletePeople(id):
+    people = People.query.filter_by(id=id).first()
+    
+    if people is not None:
+        db.session.delete(people)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Eliminación correcta de Personaje"
+        }
+        return jsonify(response_body), 200
+
+    # Afuera del if
+    response_body = {
+            "msg": "Personaje no existe"
+        }
+    return jsonify(response_body), 400
+
+# Put - People
+@app.route('/people/<int:id>', methods=['PUT'])
+def putPeople(id):
+    body = json.loads(request.data)
+    
+    people = People.query.filter_by(id=id).first()
+    
+    if people is not None:
+        people.name = body["name"]
+        people.height = body["height"]
+        people.mass = body["mass"]
+        people.hair_color = body["hair_color"]
+        people.skin_color = body["skin_color"]
+        people.eye_color = body["eye_color"]
+        people.birth_year = body["birth_year"]
+        people.gender = body["gender"]
+        people.planet_id = body["planet_id"]
+
+        db.session.add(people)
+        db.session.commit()
+
+        response_body = {
+            "msg": "El personaje fue modificado con exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "El personaje no existe en el sistema"
+        }
+    return jsonify(response_body), 400
+
+# Post - People
+@app.route('/people', methods=['POST'])
+def postPeople():
+    body = json.loads(request.data)
+    
+    people = People.query.filter_by(name=body["name"]).first() 
+    
+    if people is None:
+        newPeople = People(name=body["name"], height=body["height"], mass=body["mass"], hair_color=body["hair_color"], skin_color=body["skin_color"], eye_color=body["eye_color"], birth_year=body["birth_year"], gender=body["gender"], planet_id=body["planet_id"])
+        db.session.add(newPeople)
+        db.session.commit()
+
+        response_body = {
+            "msg": "El personaje fue creado con exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "El personaje ya existe en el sistema"
+        }
+    return jsonify(response_body), 400
+
+# ----- Endpoint Planets -----
+
+# Get - All Planets
+@app.route('/planets', methods=['GET'])
+def all_planets():
+
+    planets = Planet.query.all() 
+    all_planets = list(map(lambda item: item.serialize(), planets))
+
+    return jsonify(all_planets), 200
+
+# Get - One Planet for ID
+@app.route('/planets/<int:id>', methods=['GET'])
+def getPlanets(id):
+
+    planet = Planet.query.filter_by(id=id).first()
+
+    if planet is not None:
+        return jsonify(planet.serialize()), 200
+
+    # Afuera del if
+    response_body = {
+            "msg": "Planeta no existe"
+        }
+    return jsonify(response_body), 400
+
+# Delete - Planet
+@app.route('/planets/<int:id>', methods=['DELETE'])
+def deletePlanet(id):
+    planet = Planet.query.filter_by(id=id).first()
+    
+    if planet is not None:
+        db.session.delete(planet)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Eliminación correcta de Planeta"
+        }
+        return jsonify(response_body), 200
+
+    # Afuera del if
+    response_body = {
+            "msg": "Planeta no existe"
+        }
+    return jsonify(response_body), 400
+
+# Post - Planet
+@app.route('/planets', methods=['POST'])
+def postPlanet():
+    body = json.loads(request.data)
+    
+    planet = Planet.query.filter_by(name=body["name"]).first() 
+    
+    if planet is None:
+        newPlanet = Planet(name=body["name"], diameter=body["diameter"], rotation_period=body["rotation_period"], orbital_period=body["orbital_period"], gravity=body["gravity"], population=body["population"], climate=body["climate"], terrain=body["terrain"], surface_water=body["surface_water"])
+        db.session.add(newPlanet)
+        db.session.commit()
+
+        response_body = {
+            "msg": "El planeta fue creado con exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "El planeta ya existe en el sistema"
+        }
+    return jsonify(response_body), 400
+
+# Put - Planet
+@app.route('/planets/<int:id>', methods=['PUT'])
+def putPlanet(id):
+    body = json.loads(request.data)
+    
+    planet = Planet.query.filter_by(id=id).first()
+    
+    if planet is not None:
+        planet.name = body["name"]
+        planet.diameter = body["diameter"]
+        planet.rotation_period = body["rotation_period"]
+        planet.orbital_period = body["orbital_period"]
+        planet.gravity = body["gravity"]
+        planet.population = body["population"]
+        planet.climate = body["climate"]
+        planet.terrain = body["terrain"]
+        planet.surface_water = body["surface_water"]
+
+        db.session.add(planet)
+        db.session.commit()
+
+        response_body = {
+            "msg": "El planeta fue modificado con exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "El planeta no existe en el sistema"
+        }
+    return jsonify(response_body), 400
+
+# ----- Endpoint Vehicle -----
+
+# Get - All Vehicle
+@app.route('/vehicles', methods=['GET'])
+def all_vehicles():
+
+    vehicles = Vehicle.query.all() 
+    all_vehicles = list(map(lambda item: item.serialize(), vehicles))
+
+    return jsonify(all_vehicles), 200
+
+# Get - One Vehicle for ID
+@app.route('/vehicles/<int:id>', methods=['GET'])
+def getVehicle(id):
+
+    vehicle = Vehicle.query.filter_by(id=id).first()
+
+    if planet is not None:
+        return jsonify(planet.serialize()), 200
+
+    # Afuera del if
+    response_body = {
+            "msg": "Vehiculo no existe"
+        }
+    return jsonify(response_body), 400
+
+# Delete - Planet
+@app.route('/vehicles/<int:id>', methods=['DELETE'])
+def deleteVehicle(id):
+    vehicle = Vehicle.query.filter_by(id=id).first()
+    
+    if vehicle is not None:
+        db.session.delete(vehicle)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Eliminación correcta de Vehiculo"
+        }
+        return jsonify(response_body), 200
+
+    # Afuera del if
+    response_body = {
+            "msg": "Vehiculo no existe"
+        }
+    return jsonify(response_body), 400
+
+# Post - Vehicle
+@app.route('/vehicles', methods=['POST'])
+def postVehicle():
+    body = json.loads(request.data)
+    
+    vehicle = Vehicle.query.filter_by(name=body["name"]).first() 
+    
+    if vehicle is None:
+        newVehicle = Vehicle(name=body["name"], vehicle_class=body["vehicle_class"], manufacturer=body["manufacturer"], cost_in_credits=body["cost_in_credits"], length=body["length"], crew=body["crew"], passengers=body["passengers"], max_atmospheric_speed=body["max_atmospheric_speed"], cargo_capacity=body["cargo_capacity"], consumables=body["consumables"])
+        db.session.add(newVehicle)
+        db.session.commit()
+
+        response_body = {
+            "msg": "El vehiculo fue creado con exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "El vehiculo ya existe en el sistema"
+        }
+    return jsonify(response_body), 400
+
+# Put - Vehicle
+@app.route('/vehicles/<int:id>', methods=['PUT'])
+def putVehicle(id):
+    body = json.loads(request.data)
+    
+    vehicle = Vehicle.query.filter_by(id=id).first()
+    
+    if vehicle is not None:
+        vehicle.name = body["name"]
+        vehicle.vehicle_class = body["vehicle_class"]
+        vehicle.manufacturer = body["manufacturer"]
+        vehicle.cost_in_credits = body["cost_in_credits"]
+        vehicle.length = body["length"]
+        vehicle.crew = body["crew"]
+        vehicle.passengers = body["passengers"]
+        vehicle.max_atmospheric_speed = body["max_atmospheric_speed"]
+        vehicle.cargo_capacity = body["cargo_capacity"]
+        vehicle.consumables = body["consumables"]
+
+        db.session.add(vehicle)
+        db.session.commit()
+
+        response_body = {
+            "msg": "El vehiculo fue modificado con exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "El vehiculo no existe en el sistema"
+        }
+    return jsonify(response_body), 400
+
+# ----- Endpoint Favorite -----
+
+@app.route('/favorite', methods=['GET'])
+def all_Favorite():
+
+    favorites = Favorite.query.all() 
+    all_Favorites = list(map(lambda item: item.serialize(), favorites))
+
+    return jsonify(all_Favorites), 200
+
+# Delete - Favorite
+@app.route('/favorite/<int:id>', methods=['DELETE'])
+def deleteFavorite(id):
+    favorite = Favorite.query.filter_by(id=id).first()
+    
+    if favorite is not None:
+        db.session.delete(favorite)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Eliminación correcta de Favoritos"
+        }
+        return jsonify(response_body), 200
+
+    # Afuera del if
+    response_body = {
+            "msg": "Favoritos no existe"
+        }
+    return jsonify(response_body), 400
+
+# Get- Favorite for User
+@app.route('/users/<int:id>/favorite', methods=['GET'])
+def getUserFavorite(id):
+
+    
+    favorite = Favorite.query.filter_by(user_id=id).all()
+    all_Favorite = list(map(lambda item: item.serialize(), favorite))
+
+    return jsonify(all_Favorite), 200
+
+# Post - Planet Favorite for User
+@app.route('/users/<int:id>/favorite/planet', methods=['POST'])
+def postFavoritePlanet(id):
+    body = json.loads(request.data) 
+    
+    favoritePlanet = Favorite.query.filter_by(planet_id=body["planet_id"], user_id=id).first() 
+    
+    if favoritePlanet is None:
+        newFavorite = Favorite(user_id=id, planet_id=body["planet_id"])
+        db.session.add(newFavorite)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Planeta agregado a favorito con Exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "Ese usuario ya cuenta con ese planeta en favorito"
+        }
+    return jsonify(response_body), 400
+
+# Post - People Favorite for User
+@app.route('/users/<int:id>/favorite/people', methods=['POST'])
+def postFavoritePeople(id):
+    body = json.loads(request.data) 
+    
+    favoritePeople = Favorite.query.filter_by(people_id=body["people_id"], user_id=id).first() 
+    
+    if favoritePeople is None:
+        newFavorite = Favorite(user_id=id, people_id=body["people_id"])
+        db.session.add(newFavorite)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Personaje agregado a favorito con Exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "Ese usuario ya cuenta con ese personaje en favorito"
+        }
+    return jsonify(response_body), 400
+
+# Post - Vehicle Favorite for User
+@app.route('/users/<int:id>/favorite/vehicle', methods=['POST'])
+def postFavoriteVehicle(id):
+    body = json.loads(request.data) 
+    
+    favoriteVehicle = Favorite.query.filter_by(vehicle_id=body["vehicle_id"], user_id=id).first() 
+    
+    if favoriteVehicle is None:
+        newFavorite = Favorite(user_id=id, vehicle_id=body["vehicle_id"])
+        db.session.add(newFavorite)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Vehiculo agregado a favorito con Exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "Ese usuario ya cuenta con ese vehiculo en favorito"
+        }
+    return jsonify(response_body), 400
+
+# Delete - Planet Favorite for User
+@app.route('/users/<int:id>/favorite/planet', methods=['DELETE'])
+def deleteFavoritePlanet(id):
+    body = json.loads(request.data) 
+    
+    favoritePlanet = Favorite.query.filter_by(planet_id=body["planet_id"], user_id=id).first() 
+    
+    if favoritePlanet is not None:
+        db.session.delete(favoritePlanet)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Planeta eliminado de favorito con Exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "Ese usuario ya no cuenta con ese planeta en favorito"
+        }
+    return jsonify(response_body), 400
+
+# Delete - People Favorite for User
+@app.route('/users/<int:id>/favorite/people', methods=['DELETE'])
+def deleteFavoritePeople(id):
+    body = json.loads(request.data) 
+    
+    favoritePeople = Favorite.query.filter_by(people_id=body["people_id"], user_id=id).first() 
+    
+    if favoritePeople is not None:
+        db.session.delete(favoritePeople)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Personaje eliminado de favorito con Exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "Ese usuario ya cuenta con ese personaje en favorito"
+        }
+    return jsonify(response_body), 400
+
+# Delete - Vehicle Favorite for User
+@app.route('/users/<int:id>/favorite/vehicle', methods=['DELETE'])
+def deleteFavoriteVehicle(id):
+    body = json.loads(request.data) 
+    
+    favoriteVehicle = Favorite.query.filter_by(vehicle_id=body["vehicle_id"], user_id=id).first() 
+    
+    if favoriteVehicle is not None:
+        db.session.delete(favoriteVehicle)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Vehiculo eliminado de favorito con Exito"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "Ese usuario ya cuenta con ese vehiculo en favorito"
+        }
+    return jsonify(response_body), 400
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,144 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(250), nullable=False)
+    last_name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    favorite = db.relationship('Favorite', backref='user', lazy=True)
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email,
+            "username": self.username
             # do not serialize the password, its a security breach
         }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    gravity = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+    surface_water = db.Column(db.Integer, nullable=False)
+    favorite = db.relationship('Favorite', backref='planet', lazy=True)
+    people = db.relationship('People', backref='planet', lazy=True)
+
+
+    def __repr__(self):
+        return '<Planet %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water
+            # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    mass = db.Column(db.Integer, nullable=False)
+    hair_color = db.Column(db.String(250), nullable=False)
+    skin_color = db.Column(db.String(250), nullable=False)
+    eye_color = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"))
+    favorite = db.relationship('Favorite', backref='people', lazy=True)
+
+
+    def __repr__(self):
+        return '<People %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "planet_id": self.planet_id
+            # do not serialize the password, its a security breach
+        }
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    vehicle_class = db.Column(db.String(250), nullable=False)
+    manufacturer = db.Column(db.String(250), nullable=False)
+    cost_in_credits = db.Column(db.Integer, nullable=False)
+    length = db.Column(db.Integer, nullable=False)
+    crew = db.Column(db.Integer, nullable=False)
+    passengers = db.Column(db.Integer, nullable=False)
+    max_atmospheric_speed = db.Column(db.Integer, nullable=False)
+    cargo_capacity = db.Column(db.Integer, nullable=False)
+    consumables = db.Column(db.String(250), nullable=False)
+    favorite = db.relationship('Favorite', backref='vehicle', lazy=True)
+ 
+
+    def __repr__(self):
+        return '<Vehicle %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "vehicle_class": self.vehicle_class,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "max_atmospheric_speed": self.max_atmospheric_speed,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables
+            # do not serialize the password, its a security breach
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id"))
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"))
+    vehicle_id = db.Column(db.Integer, db.ForeignKey("vehicle.id"))
+     
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "planet_id": self.planet_id,
+            "vehicle_id": self.vehicle_id
+            # do not serialize the password, its a security breach
+        }
+
+    
